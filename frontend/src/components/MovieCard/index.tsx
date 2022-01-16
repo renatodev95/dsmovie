40ecf2a0ba15b5756cec58acab9f11ba0f2c0cf7
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2022-01-16 17:51:47 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -5,10 +5,9 @@ import MovieScore from "../MovieScore";
 
 type Props = {
   movie: Movie;
-}
+};
 
-function MovieCard({movie} : Props) {
-
+function MovieCard({ movie }: Props) {
   return (
     <div>
       <img
@@ -18,7 +17,7 @@ function MovieCard({movie} : Props) {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
+        <MovieScore count={movie.count} score={movie.score} />
         <Link to={`/form/${movie.id}`}>
           <div className="btn btn-primary dsmovie-btn">Avaliar</div>
         </Link>
